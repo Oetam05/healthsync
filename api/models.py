b5b166acc41c672b1dc5b5694ea34dcbe2c96174
Miguel Angel <miguelaltamar@uninorte.edu.co>
2023-04-23 02:06:17 -0500

    Cambios en los modelos y views

--- api/models.py
+++ api/models.py
@@ -2,16 +2,45 @@ from djongo import models
 
 
 class Users(models.Model):
+    TYPE_CHOICES = [    ('d', 'Doctor'),    ('p', 'Paciente'),]
     _id = models.ObjectIdField()
     UserId = models.CharField(max_length=15)
-    Email = models.CharField(max_length=50)
+    Email = models.EmailField(max_length=50)
     Password = models.CharField(max_length=50)
     Number = models.CharField(max_length=10)
+    Type = models.CharField(max_length=1, choices=TYPE_CHOICES)  
 
-
-class Institutions(models.Model):
+class Doctors(models.Model):
     _id = models.ObjectIdField()
+    UserId = models.CharField(max_length=15)
     Name = models.CharField(max_length=100)
     Address = models.CharField(max_length=100)
     PhoneNumber = models.CharField(max_length=10)
-    # Users= models.ManyToOneRel(User)
+    #Institutions = models.ManyToManyField('Institutions')
+    Email= models.EmailField(max_length=50)
+class Patients(models.Model):
+    _id = models.ObjectIdField()
+    UserId = models.CharField(max_length=15)
+    Name = models.CharField(max_length=100)
+    Address = models.CharField(max_length=100)
+    PhoneNumber = models.CharField(max_length=10)
+    Email= models.EmailField(max_length=50)
+"""class Institutions(models.Model):
+    _id = models.ObjectIdField()
+    Name = models.CharField(max_length=100)
+    Address = models.CharField(max_length=100)
+    PhoneNumber = models.CharField(max_length=10)"""
+   
+class Appointment(models.Model):
+    _id = models.ObjectIdField()
+    Patient = models.ForeignKey(Patients, on_delete=models.CASCADE)
+    Doctor = models.ForeignKey(Doctors, on_delete=models.CASCADE)
+    Date = models.DateField(max_length=10)
+    Time = models.TimeField(max_length=10)
+    
+
+class History(models.Model):
+    _id = models.ObjectIdField()
+    cita=models.OneToOneField(Appointment, on_delete=models.CASCADE)
+    Description = models.CharField(max_length=100)
+    Rating = models.CharField(max_length=100)
--- api/views.py
+++ api/views.py
@@ -3,39 +3,85 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
 
-from api.models import Users, Institutions
-from api.serializers import UserSerializer, InstitutionSerializer
+from api.models import *
+from api.serializers import *
 # Create your views here.
 
-
-@csrf_exempt
-def userApi(request, id=0):
+def CRUD(request, id,ob):
     if request.method == 'GET':
-        users = Users.objects.all()
-        users_serializer = UserSerializer(users, many=True)
+        if ob== 'Usuario':
+            users = Users.objects.all()
+        elif ob== 'Doctor':
+            users=Doctors.objects.all()
+        elif ob=='Paciente':
+            users=Patients.objects.all()
+        elif ob=='Cita':
+            users=Appointment.objects.all()
+        try:
+
+            data= JSONParser().parse(request)
+        except:
+            data = {}
+        try:
+            if data:
+                if ob== 'Usuario':
+                    users = Users.objects.filter(UserId=data['UserId'])
+                elif ob== 'Doctor':
+                    users=Doctors.objects.filter(UserId=data['UserId'])
+                elif ob=='Paciente':
+                    users=Patients.objects.filter(UserId=data['UserId'])
+                elif ob=='Cita':
+                    users=Appointment.objects.filter(_id=data['_id'])
+        except:
+            data = {}
+        if ob== 'Usuario':
+            users_serializer = UserSerializer(users, many=True)
+        elif ob== 'Doctor':
+            users_serializer = DoctorSerializer(users, many=True)
+        elif ob=='Paciente':
+            users_serializer = PatientSerializer(users, many=True)
+        elif ob=='Cita':
+            users_serializer = AppointmentSerializer(users, many=True)
         return JsonResponse(users_serializer.data, safe=False)
-    elif request.method == 'POST':
+    if request.method == 'POST':
         user_data = JSONParser().parse(request)
-        users_serializer = UserSerializer(data=user_data)
+        if ob== 'Usuario':
+    
+            users_serializer = UserSerializer(data=user_data)
+        elif ob== 'Doctor':
+            users_serializer = DoctorSerializer(data=user_data)
+        elif ob=='Paciente':
+            users_serializer = PatientSerializer(data=user_data)
+        elif ob=='Cita':
+            users_serializer = AppointmentSerializer(data=user_data)
         if users_serializer.is_valid():
             users_serializer.save()
             return JsonResponse("Registrado exitosamente", safe=False)
-        return JsonResponse("Fallo al registrarse", safe=False)
-    elif request.method=='PUT':
-        user_data = JSONParser().parse(request)
-        user=Users.objects.get(UserId=user_data['UserId'])
-        users_serializer=UserSerializer(user, data=user_data)
-        if users_serializer.is_valid():
-            users_serializer.save()
-            return JsonResponse("Actualizado exitosamente", safe=False)
-        return JsonResponse("Fallo al actualizar", safe=False)
-    elif request.method=='DELETE':
-        user = Users.objects.get(UserId=id)
-        if user:
-            user.delete()
-            return JsonResponse("Eliminado exitosamente", safe=False)
-        else:
-            return JsonResponse("Usuario no encontrado", safe=False)
+        return JsonResponse(users_serializer.errors, safe=False)
+
+@csrf_exempt
+def userApi(request, id=0):
+    return CRUD(request,id,"Usuario")
+@csrf_exempt
+def doctorApi(request, id=0):
+    return CRUD(request,id,"Doctor")
+@csrf_exempt
+def patientApi(request, id=0):
+    return CRUD(request,id,"Paciente")
+@csrf_exempt
+def AppointmentApi(request, id=0):
+    return CRUD(request,id,"Cita")
+        
+@csrf_exempt
+def medicalApi(request):
+    if request.method=="GET":
+        medical = History.objects.all()
+        data= JSONParser().parse(request)
+        if data['_id']:
+            medical = History.objects.filter(UserId=data['_id'])
+        History_serializer = historySerializer(medical, many=True)
+        return JsonResponse(History_serializer.data, safe=False)
+
 
 # def getRoutes(request):
 #     #Rutas de prueba
