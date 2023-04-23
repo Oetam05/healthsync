b14e7a932cb3f80bc2f369609d3cbb65217adf17
Mateo Aristizabal <mateo-05012@hotmail.com>
2023-04-23 16:26:04 -0500

    Ajustes en los modelos, corrección de nombres

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,6 @@ from django.urls import re_path
 from .import views
 
 urlpatterns=[
-    re_path(r'^user$', views.userApi),
-    re_path(r'^user/([0-9]+)$', views.userApi),
     re_path(r'^doctor$', views.doctorApi),
     re_path(r'^paciente$', views.patientApi),
     re_path(r'^cita$', views.AppointmentApi),
--- api/views.py
+++ api/views.py
@@ -9,34 +9,27 @@ from api.serializers import *
 
 def CRUD(request, id,ob):
     if request.method == 'GET':
-        if ob== 'Usuario':
-            users = Users.objects.all()
-        elif ob== 'Doctor':
-            users=Doctors.objects.all()
+        if ob== 'Doctor':
+            users=Doctor.objects.all()
         elif ob=='Paciente':
-            users=Patients.objects.all()
+            users=Patient.objects.all()
         elif ob=='Cita':
             users=Appointment.objects.all()
         try:
-
             data= JSONParser().parse(request)
         except:
             data = {}
         try:
             if data:
-                if ob== 'Usuario':
-                    users = Users.objects.filter(UserId=data['UserId'])
-                elif ob== 'Doctor':
-                    users=Doctors.objects.filter(UserId=data['UserId'])
+                if ob== 'Doctor':
+                    users=Doctor.objects.filter(user_id=data['user_id'])
                 elif ob=='Paciente':
-                    users=Patients.objects.filter(UserId=data['UserId'])
+                    users=Patient.objects.filter(user_id=data['user_id'])
                 elif ob=='Cita':
                     users=Appointment.objects.filter(_id=data['_id'])
         except:
             data = {}
-        if ob== 'Usuario':
-            users_serializer = UserSerializer(users, many=True)
-        elif ob== 'Doctor':
+        if ob== 'Doctor':
             users_serializer = DoctorSerializer(users, many=True)
         elif ob=='Paciente':
             users_serializer = PatientSerializer(users, many=True)
@@ -45,10 +38,7 @@ def CRUD(request, id,ob):
         return JsonResponse(users_serializer.data, safe=False)
     if request.method == 'POST':
         user_data = JSONParser().parse(request)
-        if ob== 'Usuario':
-    
-            users_serializer = UserSerializer(data=user_data)
-        elif ob== 'Doctor':
+        if ob== 'Doctor':
             users_serializer = DoctorSerializer(data=user_data)
         elif ob=='Paciente':
             users_serializer = PatientSerializer(data=user_data)
@@ -59,9 +49,6 @@ def CRUD(request, id,ob):
             return JsonResponse("Registrado exitosamente", safe=False)
         return JsonResponse(users_serializer.errors, safe=False)
 
-@csrf_exempt
-def userApi(request, id=0):
-    return CRUD(request,id,"Usuario")
 @csrf_exempt
 def doctorApi(request, id=0):
     return CRUD(request,id,"Doctor")
@@ -78,43 +65,6 @@ def medicalApi(request):
         medical = History.objects.all()
         data= JSONParser().parse(request)
         if data['_id']:
-            medical = History.objects.filter(UserId=data['_id'])
+            medical = History.objects.filter(_id=data['_id'])
         History_serializer = historySerializer(medical, many=True)
         return JsonResponse(History_serializer.data, safe=False)
-
-
-# def getRoutes(request):
-#     #Rutas de prueba
-#     routes = [
-#         {
-#             'Endpoint': '/notes/',
-#             'method': 'GET',
-#             'body': None,
-#             'description': 'Returns an array of notes'
-#         },
-#         {
-#             'Endpoint': '/notes/id',
-#             'method': 'GET',
-#             'body': None,
-#             'description': 'Returns a single note object'
-#         },
-#         {
-#             'Endpoint': '/notes/create/',
-#             'method': 'POST',
-#             'body': {'body': ""},
-#             'description': 'Creates new note with data sent in post request'
-#         },
-#         {
-#             'Endpoint': '/notes/id/update/',
-#             'method': 'PUT',
-#             'body': {'body': ""},
-#             'description': 'Creates an existing note with data sent in post request'
-#         },
-#         {
-#             'Endpoint': '/notes/id/delete/',
-#             'method': 'DELETE',
-#             'body': None,
-#             'description': 'Deletes and exiting note'
-#         },
-#     ]
-#     return JsonResponse(routes, safe=False)
